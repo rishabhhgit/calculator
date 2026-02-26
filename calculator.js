8e7412b7f599a965cdf69507dd6737f290977ba5
rishabh <rishabhh.steam@gmail.com>
2026-02-26 21:39:34 +0530

    first_COMMIT

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let message = "Hello, JavaScript!";
+
+// Blocks are enclosed in curly braces
+
+  console.log("This is a code block");
+
+var x = 10; // Variable declaration
+
+function greet(name) {
+  return "Hello, " + name + "!";
+}
